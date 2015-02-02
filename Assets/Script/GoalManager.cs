39ad796577ece3a3198a77785f1c35cb5610faf7
Daan Ruiter <daanruiter@live.nl>
2015-02-02 10:18:08 +0100

    Goals and shit

---
+++ Assets/Script/GoalManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GoalManager : MonoBehaviour {
+
+	public Text player01ScoreText;
+	public Text player02ScoreText;
+
+	private int player01Score;
+	private int player02Score;
+
+	private GameObject ball;
+	private Vector3 ballSpawnPos = new Vector3(0, 0, 0);
+
+	private void Start()
+	{
+		ball = GameObject.FindGameObjectWithTag("Ball");
+		ballSpawnPos = ball.transform.position;
+		player01ScoreText.text = "0";
+		player02ScoreText.text = "0";
+	}
+
+	public void Score(Player player)
+	{
+		if(player == Player.One)
+		{
+			player02Score++;
+			player02ScoreText.text = "" + player02Score;
+			RespawnBall();
+		}else{
+			player01Score++;
+			player01ScoreText.text = "" + player01Score;
+			RespawnBall();
+		}
+	}
+
+	private void RespawnBall()
+	{
+		ball.transform.position = ballSpawnPos;
+		ball.rigidbody.velocity = Vector3.zero;
+	}
+
+}
---
+++ Assets/Script/GoalTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Player
+{
+	One,
+	Two
+}
+
+public class GoalTrigger : MonoBehaviour {
+
+	public Player player;
+	private GoalManager GM;
+
+	private void Start()
+	{
+		GM = GameObject.FindGameObjectWithTag("GoalManager").GetComponent<GoalManager>();
+	}
+
+	private void OnTriggerEnter(Collider col)
+	{
+		if(col.transform.tag == "Ball")
+		{
+			GM.Score(player);
+		}
+	}
+}
